dce8e40c2f1c9be1e61f1844008488384ff183de
v_aman <aman.verma@classicinformatics.com>
2022-02-17 18:22:09 +0530

    Weekly status UI

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
+
 import ProtectedRoute from "./ProtectectedRoute";
 import Dashboard from "../app/Dashboard";
 import LandingPage from "../app/LandingPage";
@@ -15,12 +16,14 @@ import Resource from "../app/Resource";
 import Settings from "../app/Settings";
 import Timesheet from "../app/Timesheet";
 import Login from "../Login";
+import WeeklyStatus from "../app/WeeklyStatus";
 
 function App() {
   return (
     <>
       <Router>
         {/* <Switch> */}
+        <ProtectedRoute path="/weekly-status" exact component={WeeklyStatus} />
         <ProtectedRoute path="/dashboard" exact component={Dashboard} />
         <ProtectedRoute path="/" exact component={LandingPage} />
         <ProtectedRoute path="/hours-logged" exact component={HoursLogged} />
