ddd3dcdfedde347318e864c3af3604e0fb38ee11
Snow White <snow.white@classicinformatics.com>
2022-02-21 13:27:56 +0530

    dashboard done

--- src/actions/asyncActions.js
+++ src/actions/asyncActions.js
@@ -1,7 +1,7 @@
 import Store from "../redux/store";
 import * as syncActions from "./syncActions";
 import http from "../hoc/axiosClient";
-
+import axios from "axios";
 export const Authenticate = (req) => {
   //   console.log("in authenicate", req);
   //   const data = {
@@ -27,3 +27,24 @@ export const getTools = (req, res) => {
     })
     .catch((err) => {});
 };
+
+// export const getTrips = (req, res) => {
+//   let params = {
+//     adults: 1,
+//     children: 0,
+//     departDate: "2022-02-12",
+//     destination: "MEL",
+//     destinationCity: "Melbourne",
+//     infants: 0,
+//     origin: "NYC",
+//     originCity: "New York",
+//     returnDate: "2022-02-15",
+//     trip_class: "Y",
+//   };
+//   axios
+//     .post("http://localhost:3003/api/travelpayoutsApi", params)
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((err) => {});
+// };
