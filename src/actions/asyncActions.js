3e862f79d482baad727832630420785bf2c8cd15
snow-classic <snow.white@classicinformatics.com>
2022-04-29 11:06:17 +0530

    TA-75

--- src/actions/asyncActions.js
+++ src/actions/asyncActions.js
@@ -31,21 +31,13 @@ export const getWeeklyStatus = (date, filter, pageNumber) => {
       )}&limit=20&pageNumber=${pageNumber}`
     )
     .then((response) => {
-      // console.log("'''''''''''''''''''''''first'''''''''''''''''''''''",response)
       Store.dispatch(syncActions.getWeeklyStatus(response.data));
     })
     .catch((err) => {});
 };
 
 export const updateWeeklyStatus = (req, res, pageNumber) => {
-  // console.log(
-  //   `/api/projects/${req.project_id}/status/weekly?startDate=${moment(
-  //     res.strt
-  //   ).format("YYYY-MM-DD")}&endDate=${moment(res.end).format(
-  //     "YYYY-MM-DD"
-  //   )}   reeeeeeeeeeeeeeeeeeeee`,
-  //   req
-  // );
+
   http
     .put(
       `/api/projects/${req.project_id}/status/weekly?startDate=${moment(
@@ -84,54 +76,33 @@ export const getTimesheetResources = (date, id) => {
       `/api/projects/25/resources?monthYear=${date}&webtracker_project_id=${id}`
     )
     .then((response) => {
-      // console.log("data of resources : ", response.data, date, id);
-
       Store.dispatch(syncActions.getTimesheetResources(response.data));
     })
     .catch((err) => console.log(err));
 };
 
 export const getWeeklyStatusProjects = (name, id) => {
-  // console.log(name,id,"?????????????????????????????????")
   http
     .get(`/api/projects/status/weekly/searchtable?searchquery=${name}&id=${id}`)
     .then((response) => {
-      // console.log("$$$$$$$$$$$$$$$$$$$$", response.data);
       Store.dispatch(syncActions.getWeeklyStatus(response.data));
     })
     .catch((err) => {
-      // console.log("error found", err);
     });
 };
 
 export const get_health_status = () => {
   http
-    // .get(`/api/projects/status?startDate=2022-02-05&endDate=2022-02-12`)date.end
     .get(`/api/table/project_health_status`)
     .then((response) => {
-      // console.log(
-      //   `/api/projects/status/weekly?startDate=${moment(date.strt).format(
-      //     "YYYY-MM-DD"
-      //   )}&endDate=${moment(date.end).format(
-      //     "YYYY-MM-DD"
-      //   )}&engagement_type=${filter}zzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
-      // );
       Store.dispatch(syncActions.get_health_status(response.data));
     })
     .catch((err) => {});
 };
 export const get_engagement_types = () => {
   http
-    // .get(`/api/projects/status?startDate=2022-02-05&endDate=2022-02-12`)date.end
     .get(`/api/table/projects/field/engagement_type`)
     .then((response) => {
-      // console.log(
-      //   `/api/projects/status/weekly?startDate=${moment(date.strt).format(
-      //     "YYYY-MM-DD"
-      //   )}&endDate=${moment(date.end).format(
-      //     "YYYY-MM-DD"
-      //   )}&engagement_type=${filter}zzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
-      // );
       Store.dispatch(syncActions.get_engagement_types(response.data));
     })
     .catch((err) => {});
