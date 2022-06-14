c1e364b83de606dcf39180dd149b757f00350b8f
Ritvik <ritvik.bandewar@classicinformatics.com>
2022-06-14 11:22:10 +0530

    HL 5

--- src/components/app/HoursLogged/index.jsx
+++ src/components/app/HoursLogged/index.jsx
@@ -8,7 +8,7 @@ import DateFilter from "../../commonComponents/DateFilterComponent/DateFilter";
 import moment from "moment";
 import ProjectComponent from "./ProjectComp";
 import TimesheetTable from "../../commonComponents/TimesheetTable/TimesheetTable";
-import {getHoursloggedData} from "../../../redux/actions/hoursloggedAction"
+import { getHoursloggedData } from "../../../redux/actions/hoursloggedAction";
 import TimesheetFilters from "../../commonComponents/timesheetFilters/timesheetFilters";
 
 const HoursLogged = () => {
@@ -23,7 +23,6 @@ const HoursLogged = () => {
     "Status",
   ];
 
-  
   const monthFormat = "MMM YYYY";
   const filterData = [];
   const hoursLoggedModuleData = useSelector(
@@ -32,17 +31,17 @@ const HoursLogged = () => {
 
   const dispatch = useDispatch();
 
-  //to show ProjectComponent  
-  const [hoursloggedResources, setHoursloggedResources] = useState(false)
+  //to show ProjectComponent
+  const [hoursloggedResources, setHoursloggedResources] = useState(false);
 
   const [tableData, setTableData] = useState(null);
 
- //dispatches API action
+  //dispatches API action
   useEffect(() => {
     dispatch(getHoursloggedData("06/2022"));
   }, []);
 
-// sets Hours Logged Data
+  // sets Hours Logged Data
   useEffect(() => {
     if (hoursLoggedModuleData !== null) {
       hoursLoggedModuleData.forEach((ele) => {
@@ -62,41 +61,46 @@ const HoursLogged = () => {
     setTableData(filterData);
   }, [hoursLoggedModuleData]);
 
-
-
   return (
     <>
-    {hoursloggedResources ? <ProjectComponent /> : (<>
-      <div className="timesheet-container">
-        <div className="timesheet-back-button">
-          <p className="back-to-dashboard"> <span className="back-arrow"> <BackArrow /> </span> Back Dashboard</p>
+      {hoursloggedResources ? (
+        <ProjectComponent />
+      ) : (
+        <>
+          <div className="timesheet-container">
+            <div className="timesheet-back-button">
+              <p className="back-to-dashboard">
+                {" "}
+                <span className="back-arrow">
+                  {" "}
+                  <BackArrow />{" "}
+                </span>{" "}
+                Back Dashboard
+              </p>
+            </div>
+            <div className="timesheet-container-heading">
+              <div className="timesheet-heading-title">
+                <h3> Hours Logged / Project </h3>
+              </div>
+              <div className="timesheet-heading-date">
+                <DateFilter />
+              </div>
+            </div>
+            <TimesheetFilters />
 
-        </div>
-        <div className="timesheet-container-heading">
-          <div className="timesheet-heading-title">
-            <h3 > Hours Logged / Project </h3>
-
+            <div className="table-container">
+              {hoursLoggedModuleData !== null ? (
+                <TimesheetTable
+                  tableCols={tableColArray}
+                  tableData={tableData}
+                />
+              ) : null}
+            </div>
           </div>
-          <div className="timesheet-heading-date">
-            <DateFilter />
-
-          </div>
-
-        </div>
-        <TimesheetFilters />
-
-
-        <div className="table-container">
-        {hoursLoggedModuleData !== null ? <TimesheetTable tableCols={tableColArray} tableData={tableData}/>: null}
-           
-        </div>
-
-    </div>
-    </>)}
-
+        </>
+      )}
     </>
   );
 };
-
 
 export default DashboardTemplate(HoursLogged);
--- src/redux/actions/hoursloggedAction.js
+++ src/redux/actions/hoursloggedAction.js
@@ -1,15 +1,15 @@
-import axios from "axios"
-import { API_ENDPOINTS } from "../../appConfig"
-import {SET_HOURSLOGGED_DATA, SET_HOURSLOGGED_DATA_ERR} from '../type'
+import axios from "axios";
+import { API_ENDPOINTS } from "../../appConfig";
+import { SET_HOURSLOGGED_DATA, SET_HOURSLOGGED_DATA_ERR } from "../type";
 
 export const getHoursloggedData = (localDate) => {
-    return async function getHoursloggedDataThunk(dispatch) {
-        const requestUrl = `${API_ENDPOINTS.hourslogged}${localDate}&id=18`;
-        try{
-            const response = await axios.get(requestUrl);
-            dispatch({type:SET_HOURSLOGGED_DATA, payload: response})
-        }catch(err){
-            dispatch({type:SET_HOURSLOGGED_DATA_ERR, payload: err})
-        }
+  return async function getHoursloggedDataThunk(dispatch) {
+    const requestUrl = `${API_ENDPOINTS.hourslogged}${localDate}&id=18`;
+    try {
+      const response = await axios.get(requestUrl);
+      dispatch({ type: SET_HOURSLOGGED_DATA, payload: response.data });
+    } catch (err) {
+      dispatch({ type: SET_HOURSLOGGED_DATA_ERR, payload: err });
     }
-}
+  };
+};
