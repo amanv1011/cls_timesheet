920647eb978ed6aa10abbf4f20d4bc7585132232
Sahil <sahil.sondawale@classicinformatics.com>
2022-07-07 19:53:19 +0530

    updated_table_pagination

--- src/components/app/Timesheet/index.jsx
+++ src/components/app/Timesheet/index.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import timesheetLayoutTemplate from "../../layouts/timesheetLayout/timesheetLayoutTemplate";
 import TimesheetFilters from "../../commonComponents/timesheetFilters/timesheetFilters";
 import BackArrow from "../../../assets/images/icons/BackArrow";
-import TimesheetTable from "../../commonComponents/TimesheetTable/TimesheetTable"
-import './TimesheetModule.css';
+import TimesheetTable from "../../commonComponents/TimesheetTable/TimesheetTable";
+import "./TimesheetModule.css";
 import DateFilter from "../../commonComponents/DateFilterComponent/DateFilter";
 import ModalTimesheet from "../../commonComponents/Modal/ModalTimesheet";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,18 +11,23 @@ import { setModalActive } from "../../../redux/actions/modalAction";
 import { getTimesheetData } from "../../../redux/actions/timesheetActions";
 import moment from "moment";
 import { useHistory } from "react-router-dom";
-import { getTimesheetResourceData, getParticularResourceData, setResourceName } from "../../../redux/actions/timesheetResourceAction";
-import './TimesheetModule.css';
+import {
+  getTimesheetResourceData,
+  getParticularResourceData,
+  setResourceName,
+} from "../../../redux/actions/timesheetResourceAction";
+import "./TimesheetModule.css";
 
 const Timesheet = (props) => {
-  const history = useHistory()
+  const history = useHistory();
   const todaysDate = moment().format("MM/YYYY");
   const [timesheetFilterData, setTimesheetFilterData] = useState(null);
   const [timesheetResources, setTimesheetResources] = useState();
   const [filterDataMini, setFilterDataMini] = useState(null);
-  const [webtrackerId , setWebtrackerId] = useState(" ");
-  const timesheetModuleData = useSelector((state) => state.timesheet.timesheetData)
-  const [timesheetFilterSwitch, setTimesheetFilterSwitch]  = useState(false);
+  const [webtrackerId, setWebtrackerId] = useState(" ");
+  const timesheetModuleData = useSelector((state) => state.timesheet.timesheetData);
+  const [timesheetFilterSwitch, setTimesheetFilterSwitch] = useState(false);
+  const [resTableActivePage, setResTableActivePage] = useState(1);
   // const timesheetFilterSwitch = useSelector((state) => state.timesheetFilterSwitch.showSwitchTab)
 
   const timesheetStartDate = useSelector(
@@ -32,121 +37,138 @@ const Timesheet = (props) => {
     (state) => state.dateFilter.filterDateEnd
   );
 
-  const dispatch = useDispatch()
-  
-  const timesheetResourceData = useSelector((state) =>  state.timesheetResource.timesheetResourceData );
+  const dispatch = useDispatch();
 
+  const timesheetResourceData = useSelector(
+    (state) => state.timesheetResource.timesheetResourceData
+  );
 
+  // pagination functions
+
+  const resGotoNextPage = () => {
+    setResTableActivePage((page) => page + 1);
+  };
+
+  const resgotoPrevPage = () => {
+    setResTableActivePage((page) => page - 1);
+  };
+
+  const resChangePage = (event) => {
+    const pageNumber = Number(event.target.textContent);
+    setResTableActivePage(pageNumber);
+  };
 
   const backToDashboard = () => {
-    history.push("/dashboard")
-  }
+    history.push("/dashboard");
+  };
 
   const setResourcesData = (event) => {
-    setWebtrackerId(event.target.id)
-    dispatch(getTimesheetResourceData(event.target.id,timesheetStartDate,timesheetEndDate));
+    setWebtrackerId(event.target.id);
+    dispatch(
+      getTimesheetResourceData(
+        event.target.id,
+        timesheetStartDate,
+        timesheetEndDate
+      )
+    );
     setTimesheetFilterSwitch(true);
-  }
-
+  };
 
   const setResourceDetailedData = (event) => {
-    const userID = event.target.id
+    const userID = event.target.id;
     dispatch(setResourceName(event.target.innerText));
-    dispatch(getParticularResourceData(webtrackerId, timesheetStartDate, timesheetEndDate, userID))
+    dispatch(
+      getParticularResourceData(
+        webtrackerId,
+        timesheetStartDate,
+        timesheetEndDate,
+        userID
+      )
+    );
     dispatch(setModalActive());
-  }
-  
+  };
 
   const TimesheetSwitchCols = [
     {
-      columnName: 'Projects',
+      columnName: "Projects",
       columnKeyValue: "WebTrackerId",
       keyFunction: setResourcesData,
-      
     },
 
     {
-      columnName: 'Project Owner'
-    }
-  ]
+      columnName: "Project Owner",
+    },
+  ];
   const TimesheetSwitchCols2 = [
- 
     {
-      columnName: 'Resource',
+      columnName: "Resource",
       columnKeyValue: "UserId",
       keyFunction: setResourceDetailedData,
-      
     },
 
     {
-      columnName: 'Hours Logged',
-      
-    }
-  ]
+      columnName: "Hours Logged",
+    },
+  ];
   const TimesheetModalCols = [
     {
-      columnName: 'Projects',
+      columnName: "Projects",
       columnKeyValue: "WebTrackerId",
       keyFunction: setResourcesData,
     },
 
     {
-      columnName: 'Project Owner',
+      columnName: "Project Owner",
     },
     {
-      columnName: 'Project Code',
-
-    },
-
-    {
-      columnName: 'Account Code',
+      columnName: "Project Code",
     },
 
     {
-      columnName: 'Engagement Type',
+      columnName: "Account Code",
     },
 
     {
-      columnName: 'Hours Logged',
+      columnName: "Engagement Type",
     },
 
     {
-      columnName: 'Billed Hours',
+      columnName: "Hours Logged",
     },
-  ]
+
+    {
+      columnName: "Billed Hours",
+    },
+  ];
   useEffect(() => {
-    dispatch(getTimesheetData(todaysDate))
-
-  }, [])
+    dispatch(getTimesheetData(todaysDate));
+  }, []);
 
   useEffect(() => {
     const filterData = [];
-    if( timesheetResourceData.length >= 1) {
-      timesheetResourceData.forEach(( ele ) => {
+    if (timesheetResourceData.length >= 1) {
+      timesheetResourceData.forEach((ele) => {
         filterData.push({
           ResourceName: ele.name,
-          TimeLog: (parseInt(ele.time_logged))/60, 
-        })
-      })
-    } 
-    setTimesheetResources(filterData)
-    return () => { }
-
-  },[timesheetResourceData] )
+          TimeLog: parseInt(ele.time_logged) / 60,
+        });
+      });
+    }
+    setTimesheetResources(filterData);
+    return () => {};
+  }, [timesheetResourceData]);
 
   useEffect(() => {
     const filterData = [];
     const filterDataMini = [];
-    
 
     if (timesheetModuleData !== null) {
       timesheetModuleData.forEach((ele) => {
-
         filterDataMini.push({
           WebTrackerId: ele.webtracker_project_id,
           Projects: ele.project_name,
           ProjectOwner: ele.project_owner,
-        })
+        });
         filterData.push({
           WebTrackerId: ele.webtracker_project_id,
           Projects: ele.project_name,
@@ -155,15 +177,13 @@ const Timesheet = (props) => {
           AccountCode: ele.account_code,
           EngagementType: ele.engagement_type,
           HoursLogged: ele.hours_logged,
-          BilledHours: ele.billed_hours
-
-        })
-
-      })
+          BilledHours: ele.billed_hours,
+        });
+      });
     }
-    setTimesheetFilterData(filterData)
-    setFilterDataMini(filterDataMini)
-  }, [timesheetModuleData])
+    setTimesheetFilterData(filterData);
+    setFilterDataMini(filterDataMini);
+  }, [timesheetModuleData]);
 
   return (
     <>
@@ -173,49 +193,89 @@ const Timesheet = (props) => {
 
       <div className="timesheet-container">
         <div className="timesheet-back-button">
-          <p onClick={backToDashboard} className="back-to-dashboard"> <span className="back-arrow"> <BackArrow /> </span> Back to Dashboard</p>
-
+          <p onClick={backToDashboard} className="back-to-dashboard">
+            {" "}
+            <span className="back-arrow">
+              {" "}
+              <BackArrow />{" "}
+            </span>{" "}
+            Back to Dashboard
+          </p>
         </div>
         <div className="timesheet-container-heading">
           <div className="timesheet-heading-title">
-            <h3 > Timesheet </h3>
-
+            <h3> Timesheet </h3>
           </div>
           <div className="timesheet-heading-date">
             <DateFilter />
-
           </div>
-
         </div>
         <TimesheetFilters />
-        <div>
+        <div></div>
 
-        </div>
-
-        <div className="table-container"
-         style={timesheetFilterSwitch === true ? { background: "#F5F7FB" } : { background: "#FFF" }}>
-
-          {timesheetFilterSwitch === false ? (timesheetFilterData !== null ? <><TimesheetTable tableCols={TimesheetModalCols} tableData={timesheetFilterData} /></> : null) : <>
-            <div style={{ display: "flex" }}>
-              <div style={{ width: "50%", borderRadius: "15px", margin: "3px", backgroundColor: "#FFFFFF"}} >
-                {filterDataMini && <TimesheetTable tableCols={TimesheetSwitchCols} tableData={filterDataMini} />}
-              </div>
-              
-              <div style={{ width: "50%", borderRadius: "15px", margin: "3px", backgroundColor: "#FFFFFF" }}>
-              <div className="table-project-name"></div>
-                { timesheetResourceData && <TimesheetTable tableCols={TimesheetSwitchCols2} tableData={timesheetResourceData} /> }
-              </div>
-            </div>
-          </>
-
+        <div
+          className="table-container"
+          style={
+            timesheetFilterSwitch === true
+              ? { background: "#F5F7FB" }
+              : { background: "#FFF" }
           }
-          
+        >
+          {timesheetFilterSwitch === false ? (
+            timesheetFilterData !== null ? (
+              <>
+                <TimesheetTable
+                  tableCols={TimesheetModalCols}
+                  tableData={timesheetFilterData}
+                />
+              </>
+            ) : null
+          ) : (
+            <>
+              <div style={{ display: "flex" }}>
+                <div
+                  style={{
+                    width: "50%",
+                    borderRadius: "15px",
+                    margin: "3px",
+                    backgroundColor: "#FFFFFF",
+                  }}
+                >
+                  {filterDataMini && (
+                    <TimesheetTable
+                      tableCols={TimesheetSwitchCols}
+                      tableData={filterDataMini}
+                    />
+                  )}
+                </div>
+
+                <div
+                  style={{
+                    width: "50%",
+                    borderRadius: "15px",
+                    margin: "3px",
+                    backgroundColor: "#FFFFFF",
+                  }}
+                >
+                  <div className="table-project-name"></div>
+                  {timesheetResourceData && (
+                    <TimesheetTable
+                      tableCols={TimesheetSwitchCols2}
+                      tableData={timesheetResourceData}
+                      locChangePage={resChangePage}
+                      locgotoNextPage={resGotoNextPage}
+                      locgotoPrevPage={resgotoPrevPage}
+                      locCurrentPage={resTableActivePage}
+                    />
+                  )}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default timesheetLayoutTemplate(
-  Timesheet
-);
+export default timesheetLayoutTemplate(Timesheet);
--- src/components/commonComponents/TimesheetTable/TimesheetTable.jsx
+++ src/components/commonComponents/TimesheetTable/TimesheetTable.jsx
@@ -22,7 +22,7 @@ const TempTable = (props) => {
     display: "grid",
     gridTemplateColumns: girdColumnsSize,
   };
-
+  
   const tableDataComponents = [
     "ProjectHealth",
     "HoursLogged",
@@ -32,17 +32,24 @@ const TempTable = (props) => {
   ];
 
   const currentPage = useSelector((state) => state.paginationStates.activePage);
-  const dataPerPage = useSelector(
-    (state) => state.paginationStates.dataPerPage
-  );
+  const dataPerPage = useSelector((state) => state.paginationStates.dataPerPage);
 
-  const indexOfLastData = currentPage * dataPerPage;
-  const indexOfFirstData = indexOfLastData - dataPerPage;
-  const currentData = props.tableData.slice(indexOfFirstData, indexOfLastData);
+  if(props.locCurrentPage !== undefined ){
+    const indexOfLastData = props.locCurrentPage * dataPerPage ;
+    const indexOfFirstData = indexOfLastData - dataPerPage - dataPerPage;
+    var currentData = props.tableData.slice(indexOfFirstData, indexOfLastData);
+
+  }else{
+    const indexOfLastData = currentPage * dataPerPage;
+    const indexOfFirstData = indexOfLastData - dataPerPage;
+    var currentData = props.tableData.slice(indexOfFirstData, indexOfLastData);
+  }
+
+
 
   return (
     <>
-      <div className="table-container-wrapper">
+      <div style={props.tableHeight === "full" ? {height:"100%"} : null} className="table-container-wrapper">
         <div
           style={
             props.tableMinWidth === undefined ? { minWidth: "735px" } : "auto"
@@ -143,6 +150,10 @@ const TempTable = (props) => {
         dataLimit={10}
         pageLimit={3}
         localPaddingBottom={props.locPaddingBottom}
+        localChangePage={props.locChangePage}
+        localGoToPrevPage={props.locgotoPrevPage}
+        localGoToNextPage={props.locgotoNextPage}
+        localCurrentPage={props.locCurrentPage}
       />
     </>
   );
