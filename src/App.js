5a497d591096ab689720bb52f75061a885bd0659
ambar_gupta <ambar.gupta@classicinformatics.com>
2022-07-11 17:05:12 +0530

    updated token code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import "antd/dist/antd.css";
 import { connect } from "react-redux";
-import React from "react";
+import React, { useEffect } from "react";
 import Spinner from "./components/Loader";
 import Routes from "./components/Routes/";
 import history from "./hoc/history";
+import UAParser from "ua-parser-js";
+import jwt_decode from "jwt-decode";
+import { LoginStorageUserDetails } from "./assets/text";
+import { removeCookie } from "./actions/user";
 
 function App(state) {
+
+  useEffect(() => {
+    const parser = new UAParser()
+    if (localStorage.getItem(LoginStorageUserDetails)) {
+      const decodedToken = jwt_decode(JSON.parse(localStorage.getItem(LoginStorageUserDetails)).token)
+      if (decodedToken.navigator !== parser.getBrowser().name) {
+        removeCookie("token");
+        localStorage.clear();
+        window.location.reload(true);
+      }
+    }
+  }, [])
+
   return (
     <>
       {state.spin.spin && <Spinner />}
