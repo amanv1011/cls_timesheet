a0e80a7162b5dea034a8756c7691e1f1367ddd34
Sahil <sahil.sondawale@classicinformatics.com>
2022-07-22 10:11:08 +0530

    members_circles_dynamic

--- src/components/commonComponents/TimesheetTable/TableStyledComponents/membersCircles.jsx
+++ src/components/commonComponents/TimesheetTable/TableStyledComponents/membersCircles.jsx
@@ -1,20 +1,57 @@
 import React from "react";
-import "./memberCircles.css"
+import { Tooltip } from "antd";
+import "./memberCircles.css";
 
 const MembersCircles = (props) => {
-    console.log(props.members)
 
-    return(
-        <>
-        <div className="member-circles-container">
-            <div className="circle circle-one">AB</div> 
-            <div className="circle circle-two">CD</div>
-            <div className="circle circle-three">EF</div>
-            <div className="circle circle-four">GH</div>
-            <div className="circle circle-five">IJ</div>
-        </div>
-        </>
-    )
-}
+  const arrayMembers = props.members !== null ? props.members.split(",") : [];
+  const fullNameArray = [];
+  const nameArray = [];
+  for (let i = 0; i < 4; i++) {
+    if (arrayMembers[i] === undefined) {
+        fullNameArray.push("-")
+      nameArray.push("-");
+    } else {
+        fullNameArray.push(arrayMembers[i])
+      let mName = arrayMembers[i].split(" ");
+      let fName =
+        mName[0] !== undefined ? mName[0].charAt(0).toUpperCase() : null;
+      let lName =
+        mName[1] !== undefined ? mName[1].charAt(0).toUpperCase() : null;
+      let finalName = fName.concat(lName);
+      nameArray.push(finalName);
+    }
+  }
+  const remainingMembersNum = arrayMembers.length > 4 ? arrayMembers.length - 4 : "-";
+  const remainingMembersName = [];
+  if(arrayMembers.length > 4){
+    for(let i = 4; i < arrayMembers.length; i++ ){
+        remainingMembersName.push(arrayMembers[i])
+    }
+  }
+  
 
-export default MembersCircles
+  return (
+    <>
+      <div className="member-circles-container">
+        <Tooltip placement="topLeft" title={fullNameArray[0]}>
+          <div className="circle circle-one">{nameArray[0]}</div>
+        </Tooltip>
+        <Tooltip placement="topLeft" title={fullNameArray[1]}>
+          <div className="circle circle-two">{nameArray[1]}</div>
+        </Tooltip>
+        <Tooltip placement="topLeft" title={fullNameArray[2]}>
+          <div className="circle circle-three">{nameArray[2]}</div>
+        </Tooltip>
+        <Tooltip placement="topLeft" title={fullNameArray[3]}>
+          <div className="circle circle-four">{nameArray[3]}</div>
+        </Tooltip>
+        <Tooltip placement="topLeft" title={remainingMembersName.toString()}>
+          <div className="circle circle-five">{remainingMembersNum === "-" ? "-"  :"+" + remainingMembersNum}</div>
+        </Tooltip>
+      </div>
+    </>
+  );
+};
+
+export default MembersCircles;
