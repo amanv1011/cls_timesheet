ab1d3218d74b6ec9110633463c447426a5203323
Saurabh Singh <saurabhsingh@classicinformatics.com>
2022-07-28 10:20:55 +0530

    Improved_search&_shown_noDataFound

--- src/redux/actions/timesheetFilterAction.js
+++ src/redux/actions/timesheetFilterAction.js
@@ -6,23 +6,30 @@ import {
 
 
 } from "../type";
+import { SETFALSE_TIMESHEET_TABLE_DATA, SETTRUE_TIMESHEET_TABLE_DATA } from '../type'
 
 import axios from "axios";
 
 
-export const getTimesheetFilterData = ( projectName, projectOwner, engagementType, status, filterDate ) => {
+export const getTimesheetFilterData = (projectName, projectOwner, engagementType, status, filterDate) => {
 
     return async function getTimesheetFilterDataThunk(dispatch) {
         const requestUrl = `${API_ENDPOINTS.timesheetFilterReducer}monthYear=${filterDate}&project_owner_id=18&${projectName !== "" ? `projectName=${projectName}` : ""}&${projectOwner !== "" ? `projectOwnerName=${projectOwner}` : ""}&${engagementType !== "" ? `engagement_type=${engagementType}` : ""}`
-        
+
         try {
             const response = await axios.get(requestUrl);
-            dispatch({ type: SET_TIMESHEET_DATA, payload:response.data.projects});
-            
-        } catch (error) {
-            dispatch({ type: SET_TIMESHEET_DATA_ERR, payload: error});
 
+            if ((response.data.projects).length === 0) {
+                dispatch({ type: SETFALSE_TIMESHEET_TABLE_DATA })
+            } else {
+                dispatch({ type: SET_TIMESHEET_DATA, payload: response.data.projects })
+                dispatch({ type: SETTRUE_TIMESHEET_TABLE_DATA })
+            }
+
+        } catch (error) {
+            dispatch({ type: SET_TIMESHEET_DATA_ERR, payload: error });
+            dispatch({ type: SETFALSE_TIMESHEET_TABLE_DATA})
         }
-    
+
     }
 }
