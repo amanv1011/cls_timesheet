1f66312d33079d33f88e7c56c3e4dd797ebc6cac
Sahil <sahil.sondawale@classicinformatics.com>
2022-08-05 11:51:46 +0530

    fixes

--- src/components/commonComponents/TimesheetTable/TableStyledComponents/membersCircles.jsx
+++ src/components/commonComponents/TimesheetTable/TableStyledComponents/membersCircles.jsx
@@ -3,16 +3,15 @@ import { Tooltip } from "antd";
 import "./memberCircles.css";
 
 const MembersCircles = (props) => {
-
   const arrayMembers = props.members !== null ? props.members.split(",") : [];
   const fullNameArray = [];
   const nameArray = [];
   for (let i = 0; i < 4; i++) {
     if (arrayMembers[i] === undefined) {
-        fullNameArray.push("-")
+      fullNameArray.push("-");
       nameArray.push("-");
     } else {
-        fullNameArray.push(arrayMembers[i])
+      fullNameArray.push(arrayMembers[i]);
       let mName = arrayMembers[i].split(" ");
       let fName =
         mName[0] !== undefined ? mName[0].charAt(0).toUpperCase() : null;
@@ -22,14 +21,14 @@ const MembersCircles = (props) => {
       nameArray.push(finalName);
     }
   }
-  const remainingMembersNum = arrayMembers.length > 4 ? arrayMembers.length - 4 : "-";
+  const remainingMembersNum =
+    arrayMembers.length > 4 ? arrayMembers.length - 4 : "-";
   const remainingMembersName = [];
-  if(arrayMembers.length > 4){
-    for(let i = 4; i < arrayMembers.length; i++ ){
-        remainingMembersName.push(arrayMembers[i])
+  if (arrayMembers.length > 4) {
+    for (let i = 4; i < arrayMembers.length; i++) {
+      remainingMembersName.push(arrayMembers[i]);
     }
   }
-  
 
   return (
     <>
@@ -37,18 +36,31 @@ const MembersCircles = (props) => {
         <Tooltip placement="topLeft" title={fullNameArray[0]}>
           <div className="circle circle-one">{nameArray[0]}</div>
         </Tooltip>
-        <Tooltip placement="topLeft" title={fullNameArray[1]}>
-          <div className="circle circle-two">{nameArray[1]}</div>
-        </Tooltip>
-        <Tooltip placement="topLeft" title={fullNameArray[2]}>
+        {nameArray[1] !== "-" ? (
+          <Tooltip placement="topLeft" title={fullNameArray[1]}>
+            <div className="circle circle-two">{nameArray[1]}</div>
+          </Tooltip>
+        ) : null}
+        {nameArray[2] !== "-" ?        <Tooltip placement="topLeft" title={fullNameArray[2]}>
           <div className="circle circle-three">{nameArray[2]}</div>
-        </Tooltip>
-        <Tooltip placement="topLeft" title={fullNameArray[3]}>
+        </Tooltip> : null}
+        {nameArray[3] !== "-" ?         <Tooltip placement="topLeft" title={fullNameArray[3]}>
           <div className="circle circle-four">{nameArray[3]}</div>
-        </Tooltip>
-        <Tooltip placement="topLeft" title={remainingMembersName.toString()}>
-          <div className="circle circle-five">{remainingMembersNum === "-" ? "-"  :"+" + remainingMembersNum}</div>
-        </Tooltip>
+        </Tooltip> : null}
+        {remainingMembersNum !== "-" ?         <Tooltip placement="topLeft" title={remainingMembersName.toString()}>
+          <div className="circle circle-five">
+            {remainingMembersNum === "-" ? "-" : "+" + remainingMembersNum}
+          </div>
+        </Tooltip> : null}
+
+
+
+
+
+
+
+
+
       </div>
     </>
   );
